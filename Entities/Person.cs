7fd35e7656a20f0572a01406403336395c33a94f
Raman Liashneuski <romanlesh94@gmail.com>
2023-07-28 20:51:15 +0300

    Added repository, services and entities

---
+++ Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Person : BaseEntity
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+    }
+}
